52cf4a89bec235c2b333577e3977cbddeb17b206
Angelica203 <brantleyhomebuyer@gmail.com>
2020-05-28 16:28:08 -0400

    WIP: Adding initial file/folder structure

---
+++ lib/cli_project.rb
@@ -0,0 +1,8 @@
+require 'bundler/setup'
+Bundler.require(:default)
+require 'digest'
+require_relative'./cli_project/cli.rb'
+require_relative'./cli_project/api.rb'
+require_relative'./cli_project/test.rb'
+
+
---
+++ lib/cli_project/test.rb
@@ -0,0 +1,3 @@
+class Test
+  puts "test loaded"
+end
